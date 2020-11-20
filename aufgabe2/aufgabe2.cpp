7fab024492075f2d053c87f10fecb49d51fcf295
lennyk98 <lennyk98@mi.fu-berlin.de>
2020-11-20 16:09:00 +0100

    tried here and there a lot, got a working version, which is cleaner and better.

--- aufgabe2/aufgabe2.cpp
+++ aufgabe2/aufgabe2.cpp
@@ -1,6 +1,5 @@
 #include"aufgabe2.hpp"
 #include<algorithm>
-#include<iostream>
 
 void construct(std::vector<uint32_t>& sa, const std::string& text)
 {
@@ -18,46 +17,6 @@ void construct(std::vector<uint32_t>& sa, const std::string& text)
             ++b;
         }
         return text[a] < text[b];
-    });
-}
-
-
-void construct_alt(std::vector<uint32_t>& sa, const std::string& text)
-{
-    sa.clear();
-    //pushes indices from 0 to n-1 into sa
-    for(uint32_t i {}; i < text.size(); ++i) sa.push_back(i);
-    std::sort(sa.begin(), sa.end(), [&] (uint32_t a, uint32_t b)
-    {
-        bool ans = true;
-        while(a < text.length() && b < text.length())
-        {
-            //orders suffices ascending
-            if(text[a] > text[b]) { return false; }
-            if(text[a] < text[b]) { return true; }
-            //when the chars are equal we move a postion further in both strings
-            else
-            {
-                //set to treu in case, smaller suffix reaches end, and ist equal to longer
-                ++a;
-                ++b;
-            }
-       }
-       return ans;
-    });
-}
-
-void construct_speed(std::vector<uint32_t>& sa, const std::string& text)
-{
-    sa.clear();
-    //pushes indices from 0 to n-1 into sa 
-    for(uint32_t i {}; i < text.size(); ++i) sa.push_back(i);
-
-    std::sort(sa.begin(), sa.end(), [&] (uint32_t a, uint32_t b) 
-    {
-    const char* a_ptr = text.c_str() + a -1, *b_ptr = text.c_str() +b -1;
-    while(*(a_ptr++) == *(b_ptr++));
-    return *a_ptr < *b_ptr;
     });
 }
 
--- aufgabe2/aufgabe2_main.cpp
+++ aufgabe2/aufgabe2_main.cpp
@@ -1,51 +1,14 @@
 #include<iostream>
 #include"aufgabe2.cpp"
-#include<chrono>
-#include<fstream>
 
 int main(int argc, char* argv[])
 {
 
     if(argc == 2)
     {
-        //Reads fasta
-        std::ifstream  is(argv[1]);
-        if (!is.good())
-        { // read file instead of treating as text
-          std::cerr << "Cannot open file '" << argv[1] << "'\n";
-          return 1;
-        }
-
-        std::string tmp, text;
-        getline(is, tmp);  // header (optional)
-        if (tmp[0] != '>') text = tmp;
-        while (getline(is, tmp))
-        {
-          text += tmp;
-        }
-        std::cout << "read good \n" ;
-
+        //modi1
         //starts computing
-        std::vector<unsigned int> junge {};
 
-        //modi1
-        auto start = std::chrono::steady_clock::now();
-        construct(junge, text);
-        auto end = std::chrono::steady_clock::now();
-        std::cout << "sa_size: " << junge.size() << "\n text_size: " << text.size() << std::endl;
-
-        std::cout << "Elapsed time in milliseconds (smart) : "
-                  << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
-                  << " ms" << std::endl;
-        //modi1
-        std::vector<unsigned int> junge0 {};
-        auto start0 = std::chrono::steady_clock::now();
-        construct_speed(junge0, text);
-        auto end0 = std::chrono::steady_clock::now();
-
-        std::cout << "Elapsed time in milliseconds (antons SPEEEEEEEEED : "
-                  << std::chrono::duration_cast<std::chrono::milliseconds>(end0 - start0).count()
-                  << " ms" << std::endl;
     }
 
     else if(argc >2)
